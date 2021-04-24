999281cdf815931efab04b72b9bc48e3fae841cf
Zach Antosko <zachary.antosko@gmail.com>
2021-04-24 12:05:32 -0500

    added media queries

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const card = document.querySelector(".card");
 const linkMask = document.querySelector(".link-mask")
 const linkMaskText = document.querySelector(".link-mask-text")
 
-card.addEventListener("mouseover",() => {
-  linkMask.style.transition = "all 0.4s ease-in-out"
-  linkMask.style.visibility = "visible";
-  linkMaskText.style.display = "block";
-})
+// card.addEventListener("mouseover",() => {
+//   linkMask.style.visibility = "visible";
+//   linkMask.style.transition = "all 0.4s ease-in-out"
+//   linkMaskText.style.display = "block";
+// })
 
-card.addEventListener("mouseout",() => {
-  linkMask.style.transition = "all 0.4s ease-in-out"
-  linkMask.style.visibility = "hidden";
-  linkMaskText.style.display = "none";
-})
+// card.addEventListener("mouseout",() => {
+//   linkMask.style.transition = "all 0.4s ease-in-out"
+//   linkMask.style.visibility = "hidden";
+//   linkMaskText.style.display = "none";
+// })
